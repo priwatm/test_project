981a058edae998982f02941b2fa3ba782222da70
priwatm <priwatm@gmail.com>
2018-11-14 01:29:06 +0300

    test

---
+++ main.js
@@ -0,0 +1,17 @@
+function drop() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.container')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
